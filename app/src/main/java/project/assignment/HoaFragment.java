9c3cc846566ca57631b71180fd715dc805556ed7
TDH01 <trandinhhoa19999@gmail.com>
2021-03-25 22:19:05 -0400

    Run time permission

--- app/src/main/java/project/assignment/HoaFragment.java
+++ app/src/main/java/project/assignment/HoaFragment.java
@@ -1,30 +1,42 @@
 package project.assignment;
 
 import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import java.io.File;
+
 
 public class HoaFragment extends Fragment {
-    private ImageView spriteImage ;
-    private int[] filesNames = new int[]{R.drawable.frame_1 , R.drawable.frame_2 , R.drawable.frame_3 , R.drawable.frame_4 , R.drawable.frame_5 , R.drawable.frame_6};
-    private CustomDurationAnimationDrawable animDrawable ;
+    private ImageView spriteImage;
+    private int[] filesNames = new int[]{R.drawable.frame_1, R.drawable.frame_2, R.drawable.frame_3, R.drawable.frame_4, R.drawable.frame_5, R.drawable.frame_6};
+    private CustomDurationAnimationDrawable animDrawable;
     private Drawable drawable1, drawable2, drawable3, drawable4, drawable5, drawable6;
-    private Button startDuration200 , startDuration400 ,startDuration600 ,startDuration800 ,startPermission;
+    private Button startDuration200, startDuration400, startDuration600, startDuration800, startPermission;
+    private int STORAGE_PERMISSION_CODE = 1;
 
     @Nullable
     @Override
@@ -34,7 +46,7 @@ public class HoaFragment extends Fragment {
         return rootView;
     }
 
-    private void initView(View view){
+    private void initView(View view) {
         spriteImage = view.findViewById(R.id.sprite_image);
         startDuration200 = view.findViewById(R.id.btnStartDuration200);
         startDuration400 = view.findViewById(R.id.btnStartDuration400);
@@ -48,18 +60,61 @@ public class HoaFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        startDuration200.setOnClickListener(view1 -> { startDuration(200); });
-        startDuration400.setOnClickListener(view1 -> { startDuration(400); });
-        startDuration600.setOnClickListener(view1 -> { startDuration(600); });
-        startDuration800.setOnClickListener(view1 -> { startDuration(800); });
+        startDuration200.setOnClickListener(view1 -> {
+            startDuration(200);
+        });
+        startDuration400.setOnClickListener(view1 -> {
+            startDuration(400);
+        });
+        startDuration600.setOnClickListener(view1 -> {
+            startDuration(600);
+        });
+        startDuration800.setOnClickListener(view1 -> {
+            startDuration(800);
+        });
         startPermission.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                requestPermissions(new String[]{Manifest.permission.CAMERA}, 1);
+
+
+                if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivity(intent);
+                    Toast.makeText(getActivity(), "You have already granted this permission!", Toast.LENGTH_LONG).show();
+
+                } else {
+                    requestStoragePermission();
+                }
+
             }
         });
     }
-    private void startDuration(int duration){
+
+    private void requestStoragePermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            new AlertDialog.Builder(getActivity())
+                    .setTitle("Permission needed")
+                    .setMessage("This permission is need because of this and that")
+                    .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+
+                        }
+                    })
+                    .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    })
+                    .create().show();
+        } else {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+        }
+    }
+
+    private void startDuration(int duration) {
         animDrawable = new CustomDurationAnimationDrawable();
         animDrawable.stop();
         drawable1 = requireActivity().getDrawable(R.drawable.frame_1);
@@ -86,12 +141,12 @@ public class HoaFragment extends Fragment {
             case 1: {
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
+
                 } else {
                     // permission denied, boo! Disable the
                     // functionality that depends on this permission.
-                    Snackbar.make(startPermission,requireActivity().getString(R.string.denied),Snackbar.LENGTH_LONG).show();
+                    Snackbar.make(startPermission, requireActivity().getString(R.string.denied), Snackbar.LENGTH_LONG).show();
+
                 }
                 return;
             }
